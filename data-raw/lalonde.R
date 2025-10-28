a870841c29ba373500e1b027aa91268f1b39215c
Matthew Pancia <=>
2025-10-28 00:13:09 -0700

    Add air for formatting.

--- data-raw/lalonde.R
+++ data-raw/lalonde.R
@@ -1,8 +1,15 @@
 library(readr)
 
 if (!file.exists("data-raw/lalonde.txt")) {
-  download.file("https://dataverse.harvard.edu/api/access/datafile/7440281", "data-raw/lalonde.txt")
+  download.file(
+    "https://dataverse.harvard.edu/api/access/datafile/7440281",
+    "data-raw/lalonde.txt"
+  )
 }
 
-lalonde <- read_delim("data-raw/lalonde.txt", delim = " ", col_types = "liillllnnn")
+lalonde <- read_delim(
+  "data-raw/lalonde.txt",
+  delim = " ",
+  col_types = "liillllnnn"
+)
 usethis::use_data(lalonde, overwrite = TRUE)
